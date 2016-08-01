cd17722749449fc2f50a4f9e0dd2d4fa665e4861
Xiaoxing Ye <ye@xiaoxing.us>
2016-08-01 23:28:57 +0800

    fix: no list

--- fileListGenerator.py
+++ fileListGenerator.py
@@ -23,7 +23,7 @@ def log(log_msg):
 
 def log_finish():
     with open(log_path, "w") as handler:
-        handler.write("\n".join(log_list))
+        handler.write("\n".join([" ".join(i) for i in log_list]))
 
 def createDict(path, root={}):
     pathList = listdir(path)
